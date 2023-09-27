0ce2bfe6a2f5b13a7e5872f4e36836a5aac7966f
Zachary Chen <zacchenzy@gmail.com>
2023-09-27 09:54:32 -0400

    update

--- project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
+++ project/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h
@@ -21,7 +21,7 @@
 #define __STM32F4xx_HAL_DMA_EX_H
 
 #ifdef __cplusplus
-extern "C" {
+ extern "C" {
 #endif
 
 /* Includes ------------------------------------------------------------------*/
@@ -33,21 +33,22 @@ extern "C" {
 
 /** @addtogroup DMAEx
   * @{
-  */
+  */ 
 
 /* Exported types ------------------------------------------------------------*/
 /** @defgroup DMAEx_Exported_Types DMAEx Exported Types
   * @brief DMAEx Exported types
   * @{
   */
-
+   
 /** 
   * @brief  HAL DMA Memory definition  
-  */
-typedef enum {
-    MEMORY0 = 0x00U, /*!< Memory 0     */
-    MEMORY1 = 0x01U  /*!< Memory 1     */
-} HAL_DMA_MemoryTypeDef;
+  */ 
+typedef enum
+{
+  MEMORY0      = 0x00U,    /*!< Memory 0     */
+  MEMORY1      = 0x01U     /*!< Memory 1     */
+}HAL_DMA_MemoryTypeDef;
 
 /**
   * @}
@@ -75,7 +76,7 @@ HAL_StatusTypeDef HAL_DMAEx_ChangeMemory(DMA_HandleTypeDef *hdma, uint32_t Addre
 /**
   * @}
   */
-
+         
 /* Private functions ---------------------------------------------------------*/
 /** @defgroup DMAEx_Private_Functions DMAEx Private Functions
   * @brief DMAEx Private functions
@@ -98,3 +99,4 @@ HAL_StatusTypeDef HAL_DMAEx_ChangeMemory(DMA_HandleTypeDef *hdma, uint32_t Addre
 #endif
 
 #endif /*__STM32F4xx_HAL_DMA_EX_H*/
+
